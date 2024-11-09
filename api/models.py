1b444be280cf4e0044758c0db26fd547d38ac1c4
Sudiptto Biswas <biswassudiptto@gmail.com>
2024-11-09 15:22:46 -0500

    added documentation site

--- api/models.py
+++ api/models.py
@@ -52,3 +52,21 @@ class Recruiter(db.Model):
 
     def verify_password(self, password):
         return bcrypt.check_password_hash(self.password_hash, password)
+
+
+# for innovation challenge posting from recruiter
+class InnovationChallenge(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.String(150), nullable=False)
+    location = db.Column(db.String(150), nullable=False)
+    # either 6 - 8 team size integer
+    teamSize = db.Column(db.Integer, nullable=False)
+
+    date_started = db.Column(db.DateTime, nullable=False)
+    date_ended = db.Column(db.DateTime, nullable=False)
+
+    # foreign key -> recruiter_id (recruiter who posted the challenge) 
+    recruiter_id = db.Column(db.Integer, db.ForeignKey('recruiter.id'), nullable=False)
+    recruiter = db.relationship('Recruiter', backref='innovation_challenge')
