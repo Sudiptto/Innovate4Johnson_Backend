f8cf57ae9c39c7016ae41e312269d69dc48cb8e4
Evandabest <evanhaque1@gmail.com>
2024-11-10 09:01:59 -0500

    updated import for db

--- api/kmeans.py
+++ api/kmeans.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, jsonify
 from flask_jwt_extended import create_access_token, jwt_required
 from .models import *
-
+from . import db
 import boto3
 import requests
 from io import BytesIO
